80ea02fc158b4bce2023f04088ef8d0aaa80cac2
include-jacopo <jacopopati@gmail.com>
2021-09-11 12:30:53 +0200

    aggiunta la libreria libical

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <QBoxLayout>
 #include "Calendar.h"
 #include "MainWindow.h"
+#include <libical/ical.h>
 
 int main(int argc, char *argv[]) {
+    icalcomponent *event; //prova di utilizzo
+
     QApplication a(argc, argv);
     MainWindow mainW = MainWindow();
     mainW.show();
