1679d627e8d65997ded21003c80d4b6b6828e0ca
include-jacopo <jacopopati@gmail.com>
2021-09-15 13:13:31 +0200

    fix cmakelist

--- backend/session.cpp
+++ backend/session.cpp
@@ -12,9 +12,14 @@ int new_session() {
     ne_session *sess;
     sess = ne_session_create("http", "www.google.com", 80);
 
-    ne_request
+    ne_request *req = ne_request_create(sess, "GET", "");
+    //returns a pointer to a request object
 
-
+    if (ne_request_dispatch(req)) {
+        cout << "Request failed" << ne_get_error(sess);
+    } else {
+        cout << "ok";
+    }
 
     return 1;
 }
