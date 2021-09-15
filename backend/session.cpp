5b37e18230e0a43dfed9e654841c48588e6e9596
hydroSs1 <michelepierro9548@gmail.com>
2021-09-15 17:01:48 +0200

    cbReader

--- backend/session.cpp
+++ backend/session.cpp
@@ -6,7 +6,17 @@
 #include <neon/ne_session.h>
 #include <neon/ne_request.h>
 #include <iostream>
+#include <neon/ne_207.h>
 using namespace std;
+
+int cbReader(void *userdata, const char *buf, size_t len){
+    //__PRINTFUNC__;
+
+    ne_xml_parser *parser = (ne_xml_parser *)userdata;
+    ne_xml_parse(parser, buf, len);
+
+    return 0;
+}
 
 int new_session() {
     ne_session *sess;
